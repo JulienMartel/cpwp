58417d2fd9c6e59aaeaba86753ff791a1bf57f64
ju <julienbmartel@gmail.com>
2022-11-14 20:41:27 -0600

    refined

--- pages/api/petID.js
+++ pages/api/petID.js
@@ -1,65 +1,70 @@
-import contract_abi from './../../CpContractABI.json'
-import Web3 from 'web3'
-import sharp from "sharp"
+import contract_abi from "./../../CpContractABI.json";
+import Web3 from "web3";
+import sharp from "sharp";
 
 const elementBGs = {
   grass: "#caf2b5",
   water: "#b8eeff",
   fire: "#fabf88",
   air: "#cdcdf5",
-  egg: "#ffffff"
-}
+  egg: "#ffffff",
+};
 
-
-const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/cd2f36d854094edcbf3ee4016d19d874"))
-const petsContract = new web3.eth.Contract(contract_abi, "0x86C10D10ECa1Fca9DAF87a279ABCcabe0063F247")
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(process.env.INFURA_ENDPOINT)
+);
+const petsContract = new web3.eth.Contract(
+  contract_abi,
+  "0x86C10D10ECa1Fca9DAF87a279ABCcabe0063F247"
+);
 
 export default async function handler(req, res) {
-  const { id, size } = req.query
+  const { id, size } = req.query;
 
-  const resizeSize = size === "phone" ? 250 : 250
-  const width = size === "phone" ? 828 : 1920
-  const height = size === "phone" ? 1792 : 1080
+  const resizeSize = size === "phone" ? 250 : 250;
+  const width = size === "phone" ? 828 : 1920;
+  const height = size === "phone" ? 1792 : 1080;
 
-  let uri
+  let uri;
   try {
-    uri = await petsContract.methods.tokenURI(id).call()
+    uri = await petsContract.methods.tokenURI(id).call();
   } catch (e) {
-    return res.status(500).json({error: `Pet ${id} has not been claimed yet`})
+    return res
+      .status(500)
+      .json({ error: `Pet ${id} has not been claimed yet` });
   }
 
-  const result = await fetch(uri)
-  const { image, attributes } = await result.json()
+  try {
+    const result = await fetch(uri);
+    const { image, attributes } = await result.json();
 
-  const imgResult = await fetch(image)
-  const imgBlog = await imgResult.blob()
-  
-  const imgArr = new Uint8Array(await imgBlog.arrayBuffer())
-  
-  const resizedImgArr = new Uint8Array(
-    await sharp(imgArr)
-      .resize(resizeSize, resizeSize)
-      .toBuffer()
-  )
-    
-  const element = attributes.filter(a => a.trait_type === 'Element')[0]?.value || "egg"
+    const imgResult = await fetch(image);
+    const imgBlog = await imgResult.blob();
 
-  try { // image creation + composition
+    const imgArr = new Uint8Array(await imgBlog.arrayBuffer());
+
+    const resizedImgArr = new Uint8Array(
+      await sharp(imgArr).resize(resizeSize, resizeSize).toBuffer()
+    );
+
+    const element =
+      attributes.filter((a) => a.trait_type === "Element")[0]?.value || "egg";
+
+    // image creation + composition
     const wallpaperBuffer = await sharp({
       create: {
-        width, 
-        height, 
-        channels: 3, 
+        width,
+        height,
+        channels: 3,
         background: elementBGs[element],
-      }
+      },
     })
       .composite([{ input: resizedImgArr }])
       .png()
-      .toBuffer()
+      .toBuffer();
 
-      res.status(200).json(wallpaperBuffer.toJSON())
-  } catch (e) {
-    throw e
+    return res.status(200).json(wallpaperBuffer.toJSON());
+  } catch (error) {
+    return res.status(500).json({ error });
   }
-  
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import { 
-  Button, 
-  Container, 
-  Heading, 
-  Input, 
-  Stack, 
-  Text, 
-  HStack, 
-  Image as ChakraImg, 
-  Box, 
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  HStack,
+  Image as ChakraImg,
+  Box,
   Flex,
   useToast,
   Spinner,
@@ -16,105 +15,130 @@ import {
   Radio,
   NumberInput,
   NumberInputField,
-  NumberInputStepper,
-
-} from '@chakra-ui/react'
-import Image from 'next/image'
+} from "@chakra-ui/react";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
-  const [petID, setPetID] = useState("")
-  const [image, setImage] = useState()
-  const [loading, setLoading] = useState(false)
-  const [size, setSize] = useState("phone")
-  const [lastSize, setLastSize] = useState()
-  const toast = useToast()
+  const [petID, setPetID] = useState("");
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [size, setSize] = useState("phone");
+  const [lastSize, setLastSize] = useState();
+  const toast = useToast();
 
-  const generate = async e => {
-    e.preventDefault()
-    setLoading(true)
+  const generate = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    if (petID < 0 || petID > 20000) { 
-      setLoading(false)
+    // @ts-ignore
+    if (petID < 0 || petID > 20000) {
+      setLoading(false);
       return toast({
         title: "ID must be between 0 and 20,000",
-        status: 'error',
+        status: "error",
         duration: 5000,
         isClosable: true,
-      })
+      });
     }
 
     if (petID) {
-      const result = await fetch(`/api/petID?id=${petID}&size=${size}`)
-      const { error, ...arrBuffer } = await result.json()
+      const result = await fetch(`/api/petID?id=${petID}&size=${size}`);
+      const { error, ...arrBuffer } = await result.json();
 
       if (error) {
-        setLoading(false)
+        setLoading(false);
         return toast({
           title: error,
-          status: 'error',
+          status: "error",
           duration: 5000,
           isClosable: true,
-        })
+        });
       }
-      
-      const imgLink = new Buffer.from(arrBuffer).toString("base64")
 
-      setImage("data:image/png;base64," + imgLink)
-      setLastSize(size)
+      const imgLink = Buffer.from(arrBuffer).toString("base64");
+
+      setImage("data:image/png;base64," + imgLink);
+      // @ts-ignore
+      setLastSize(size);
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
-  return <Container my='5' maxW="container.sm">
-    <Stack spacing={4}>
-      <Heading size='lg' >Cool Pets wallpaper creator</Heading>
+  return (
+    <Container my="5" maxW="container.sm">
+      <Stack spacing={4}>
+        <Heading size="lg">Cool Pets wallpaper creator</Heading>
 
-      <Text>Create a phone or desktop wallpaper. Choose which you want, input your pet ID #, and click generate.</Text>
+        <Text>
+          Create a phone or desktop wallpaper. Choose which you want, input your
+          pet ID #, and click generate.
+        </Text>
 
-      <Stack spacing={4} as="form" onSubmit={generate}>
-        <RadioGroup size="lg" onChange={v => setSize(v)} value={size}>
-          <HStack>
-            <Radio value='phone'>Phone</Radio>
-            <Radio value='desktop'>Desktop</Radio>
+        <Stack spacing={4} as="form" onSubmit={generate}>
+          <RadioGroup size="lg" onChange={(v) => setSize(v)} value={size}>
+            <HStack>
+              <Radio value="phone">Phone</Radio>
+              <Radio value="desktop">Desktop</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <HStack w="full">
+            <NumberInput
+              min={0}
+              max={20000}
+              size="lg"
+              value={petID}
+              onChange={setPetID}
+            >
+              <NumberInputField placeholder="Pet ID (ex: 5634)" />
+            </NumberInput>
+
+            <Button isLoading={loading} size="lg" minW="60%" type="submit">
+              Generate
+            </Button>
           </HStack>
-        </RadioGroup>
+        </Stack>
 
-        <HStack w='full'>
-          <NumberInput size="lg" value={petID} onChange={v => setPetID(v)}>
-            <NumberInputField placeholder='Pet ID' />
-          </NumberInput>
+        <Box hidden={!image}>
+          <Text mb={2} color="gray">
+            On mobile, long-press the image and save to photos. On desktop,
+            right-click save ;)
+          </Text>
 
-          <Button isLoading={loading} size="lg" minW='60%' onClick={generate} >Generate</Button>  
-        </HStack>
+          <Box
+            hidden={loading}
+            shadow="2xl"
+            maxW={lastSize === "desktop" ? "100%" : "50%"}
+            mx="auto"
+          >
+            <ChakraImg border="1px solid" src={image} borderColor="gray.400" />
+          </Box>
+        </Box>
+
+        <Flex minH="400px" justify="center" hidden={!loading} align="center">
+          <Spinner size="xl" />
+        </Flex>
+
+        <CreatedBy />
       </Stack>
-
-      <Box hidden={!image}>
-        <Text mb={2} color='gray' >
-          {"On mobile, long-press the image and save to photos. On desktop, right-click save ;)"} 
-        </Text>
-        
-        <Box hidden={loading} shadow='2xl' maxW={lastSize === "desktop" ? "100%" : "50%"} mx='auto' >
-          <ChakraImg border='1px solid' src={image} borderColor="gray.400" />
-        </Box>
-
-      </Box>
-
-      <Flex minH="400px" justify="center" hidden={!loading} align="center">
-        <Spinner size="xl" />
-      </Flex>
-
-      <Flex bg="white" align="flex-end" pos='fixed' right={0} bottom={0} px={2}>
-        <Text color='gray' fontWeight={500} fontSize='2xl' verticalAlign='center' mr="1">
-          created by ju
-        </Text>
-        <Box mb={-1.5}>
-          <Image src='/no-bg-small-4888.png' width={40} height={40} />
-        </Box>
-      </Flex>
-
-    </Stack>
-  </Container>
+    </Container>
+  );
 }
 
-// unclaimed egg err message
-// general err msg
+const CreatedBy = () => (
+  <Flex bg="white" align="flex-end" pos="fixed" right={0} bottom={0} px={2}>
+    <Text
+      color="gray"
+      fontWeight={500}
+      fontSize="2xl"
+      verticalAlign="center"
+      mr="1"
+    >
+      created by ju
+    </Text>
+    <Box mb={-1.5}>
+      <Image src="/no-bg-small-4888.png" width={40} height={40} />
+    </Box>
+  </Flex>
+);
